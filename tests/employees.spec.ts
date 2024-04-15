c0c13f067fc2fbf25412c3dc6b5f462ad6729dc6
MarcusPindardBaden <pindardbadenmarcus@gmail.com>
2024-04-15 12:09:29 +0100

    adding tests for user unable to add employee, and checking created employees appear (in progress)

--- tests/employees.spec.ts
+++ tests/employees.spec.ts
@@ -31,27 +31,49 @@ test.beforeEach(async({page}) =>{
 
 
 
-test('User can add employee without optional details', async({page}) =>{
-    await page.getByRole('button', {name: 'Add employee'}).click();
-    await page.getByLabel('First name').fill('All');
-    await page.getByLabel('Last name').fill('Details');
-    await page.getByLabel('Email address').fill('pindardbadenmarcus+keydeets@gmail.com');
-    await page.getByRole('button', {name: 'Save new employee'}).click();
-    let modal = await page.getByTestId('background');
-    let modalTitle = await modal.getByRole('heading', {name: 'Success! New employee added'});
-    await expect.soft(modalTitle).toBeVisible();
-    await page.getByRole('button', {name: 'Close modal'}).click();
-});
-
-
-
-// test('User cannot add employee with missing critical details', async({page}) =>{
+// test('User can add employee without optional details', async({page}) =>{
 //     await page.getByRole('button', {name: 'Add employee'}).click();
+//     await page.getByLabel('First name').fill('Key');
+//     await page.getByLabel('Last name').fill('Details');
+//     await page.getByLabel('Email address').fill('pindardbadenmarcus+keydeets@gmail.com');
+//     await page.getByRole('button', {name: 'Save new employee'}).click();
+//     let modal = await page.getByTestId('background');
+//     let modalTitle = await modal.getByRole('heading', {name: 'Success! New employee added'});
+//     await expect.soft(modalTitle).toBeVisible();
+//     await page.getByRole('button', {name: 'Close modal'}).click();
 // });
 
-// test('User can see newly created employees', async({page}) =>{
 
+
+// test('User cannot add employee with missing first name', async({page}) =>{
+//     await page.getByRole('button', {name: 'Add employee'}).click();
+//     await page.getByLabel('Last name').fill('Details');
+//     await page.getByLabel('Email address').fill('pindardbadenmarcus+missingdeets@gmail.com');
+//     await expect.soft(page.getByRole('button', {name: 'Save new employee'})).toBeDisabled();
+//     await page.getByRole('button', {name: 'Close modal'}).click();
 // });
+
+// test('User cannot add employee with missing last name', async({page}) =>{
+//     await page.getByRole('button', {name: 'Add employee'}).click();
+//     await page.getByLabel('First name').fill('Missing');
+//     await page.getByLabel('Email address').fill('pindardbadenmarcus+missingdeets@gmail.com');
+//     await expect.soft(page.getByRole('button', {name: 'Save new employee'})).toBeDisabled();
+//     await page.getByRole('button', {name: 'Close modal'}).click();
+// });
+
+// test('User cannot add employee with missing email', async({page}) =>{
+//     await page.getByRole('button', {name: 'Add employee'}).click();
+//     await page.getByLabel('First name').fill('Missing');
+//     await page.getByLabel('Last name').fill('Details');
+//     await expect.soft(page.getByRole('button', {name: 'Save new employee'})).toBeDisabled();
+//     await page.getByRole('button', {name: 'Close modal'}).click();
+// });
+
+test('User can see key details employee', async({page}) =>{
+    let card = page.locator('div', {has: page.getByRole('heading', {name: "Key Details"})});
+    await card.getByTestId('EditButton').click();
+    await expect.soft(page.getByLabel('Email address')).toHaveValue("pindardbadenmarcus+keydeets@gmail.com");
+});
 
 // test('User can delete newly created employees', async({page}) =>{
 
@@ -63,7 +85,3 @@ test.afterEach(async({page}) =>{
     await page.getByRole('link', {name: 'Log in'});
 })
 
-
-// test('User can ', async({}) =>{
-
-// });
